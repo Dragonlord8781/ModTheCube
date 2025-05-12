f3f0fa22f46f22a54dd00ec0d291d53f594d0aca
Dragonlord8781 <evan.stone638@gmail.com>
2025-05-11 22:16:51 -0500

    finished?
    add random position

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -11,8 +11,14 @@ public class Cube : MonoBehaviour
     private float randomB;
     private float randomA;
 
+    private float randomX;
+    private float randomY;
+    private float randomZ;
+
     private float startDelay = 5.0f; // start delay
     private float switchInterval; // spawn delay
+
+    private Vector3 newPos;
 
     void Start()
     {
@@ -21,24 +27,29 @@ public class Cube : MonoBehaviour
         randomB = Random.value;
         randomA = Random.value;
 
+        randomX = Random.Range(2, 7);
+        randomY = Random.Range(2, 9);
+        randomZ = Random.Range(-4, 7);
+
         switchInterval = Random.Range(1, 10);
 
-        transform.position = new Vector3(3, 4, 1);
+        transform.position = new Vector3(randomX, randomY, randomZ);
         transform.localScale = Vector3.one * 1.3f;
         
         Material material = Renderer.material;
         
         material.color = new Color(randomR, randomG, randomB, randomA);
 
-        InvokeRepeating("RandomColor", startDelay, switchInterval);
+        InvokeRepeating("RandomForms", startDelay, switchInterval);
         InvokeRepeating("RandomValues", startDelay, switchInterval);
     }
     
-    void RandomColor()
+    void RandomForms()
     {
         Material material = Renderer.material;
 
         material.color = new Color(randomR, randomG, randomB, randomA);
+        transform.position = new Vector3(randomX, randomY, randomZ);
     }
 
     void RandomValues()
@@ -47,7 +58,12 @@ public class Cube : MonoBehaviour
         randomG = Random.value;
         randomB = Random.value;
         randomA = Random.value;
+
+        randomX = Random.Range(2, 7);
+        randomY = Random.Range(2, 9);
+        randomZ = Random.Range(-4, 7);
     }
+
     void Update()
     {
         transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
